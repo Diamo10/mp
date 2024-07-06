8d6455a55c26fdb0778f31e360098fb8a87a0bfb
Sugam Gelal <sugamgelal@Sugams-MacBook-Air.local>
2024-07-06 19:18:18 +0545

    setup database

---
+++ backend/utils/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  } catch (err) {
+    console.log("error: " + err);
+  }
+};
+
+export default connectDB;
