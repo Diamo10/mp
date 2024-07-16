fcc0017489c70272f4b71dae5c4a4fd3a34ed2e2
PonisaBaral07 <PonisaBarla1007@gmail.com>
2024-07-16 14:59:15 +0545

    working in branch

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,7 @@ connectDB();
 
 app.use("/api/project", projectController);
 console.log("hello world");
+console.log("working on branch");
 
 app.listen(process.env.PORT, () => {
   console.log("listening on port " + process.env.PORT);
