f6718b89f2aadd7f1ba79a55733639521c776c19
Khusbu Yadav <khuyadav65@gmail.com>
2024-07-22 11:12:26 +0545

    jdhjas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import NotFound from "./pages/NotFound";
+import NotFound from "./pages/Signup";
 
 const App = () => {
   return (
