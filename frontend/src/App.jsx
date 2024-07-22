374db00c79ee474f78bce56396a16d4ae5c045bc
shrutee2428 <megha1mgh@gmail.com>
2024-07-22 14:00:36 +0545

    hdaa

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,27 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
+
 import Login from "./pages/Login";
 
+import Signup from "./pages/Signup";
+
+
 const App = () => {
+  console.log("Hello World");
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+<<<<<<< HEAD
         <Route path="/login" element={<Login />} />
+=======
+
+        <Route path="/" element={<Login />} />
+
+        <Route path="/register" element={<Signup />} />
+
+>>>>>>> a0b49ce75d61c4dd2afadbe0ef28d94aa586a388
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
