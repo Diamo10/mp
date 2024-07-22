cebaed6cb61da70886ae9749d0c175037f11fcca
Sugam Gelal <sugamgelal@Sugams-MacBook-Air.local>
2024-07-22 12:05:24 +0545

    Restarted frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 
 const App = () => {
@@ -9,7 +8,6 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
